fc16971b46173f6ebe17eee618a0a118d2be50b8
Allen Kileo <96255927+Kihan1@users.noreply.github.com>
2021-12-21 10:14:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+#install the module used for text to speech conversion
 import pyttsx3
+#init function to get the engine instance in order to satisfy the speech conversion process
 engine=pyttsx3.init()
+#here input any text you want to convert to audio
 text="Walter Anthony, welcome home"
 engine.say(text)
+#here the text is being processed
 engine.runAndWait()
-rate=engine.getProperty("rate")
-print(rate)
-engine.say(text)
-engine.runAndWait
